a3778ae4599c8592592760e4a3b3da589273e238
Naresh <nareshr@vgroupinc.com>
2024-01-10 12:57:26 +0530

    master-SNAPSHOT

--- app/src/main/java/com/vgroup/testdemo/MainActivity.kt
+++ app/src/main/java/com/vgroup/testdemo/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.vgroup.testdemo
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.vgroup.mylibrary.MyLibClass
+import com.google.android.material.snackbar.Snackbar
 import com.vgroup.testdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
